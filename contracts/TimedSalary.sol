ff90d143c97a4ef1201b08932bfaa992aade9861
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2022-07-24 18:02:49 +0100

    Changed Somethings

--- contracts/TimedSalary.sol
+++ contracts/TimedSalary.sol
@@ -4,28 +4,21 @@ import "./Ownable.sol";
 contract TimedSalary is Ownable{
     using SafeMath for uint;
     uint private cooldown;
-    uint private timePaid = block.timePaid;
+    uint private timePaid = block.timestamp;
     mapping(address => uint)balances;
     mapping(address => bool)isWorker;
     mapping(address => uint)balanceRecieved;
     address[]Workers;
-    bool Paid;
 
-    function RecieveETH()external onlyOwner payable returns(bool success){
+    function PaySalaries()external onlyOwner payable returns(bool success){
      require(Workers.length != 0,"Add some workers first");
      require(cooldown != 0,"Set a Cooldown First");
-     require(Paid == false,"Salaries have not been paid yet");
      require(address(this).balance != 0,"Deposit Salaries First");
     if(block.timestamp >= timePaid.add(cooldown)){
      for(uint i = 0; i<Workers.length;i++){
-     balances[Workers[i]] = msg.value.div(Workers.length);
-     balanceRecieved[Workers[i]] = balanceRecieved[Workers[i]].add(balances[Workers[i]]);
-     balances[Workers[i]] = 0;
-     payable(Workers[i]).send(balances[Workers[i]]); 
-     Paid = true;      
+     payable(Workers[i]).send(msg.value.div(Workers.length));
      } 
     }
-    Paid = false;
     return true;
     }
 
@@ -36,15 +29,15 @@ contract TimedSalary is Ownable{
     }
 
     function AddWorkers(address _worker)external onlyOwner returns(bool success){
-        require(Paid == false,"Pay Salaries First");
+        require(address(this).balance == 0,"Contract must be empty");
         require(isWorker[_worker] == false,"Already a worker");
         Workers.push(_worker);
         isWorker[_worker] = true;
         return true;
     }
     function RemoveWorker(address _worker)external onlyOwner returns(bool success){
-        require(Paid == false,"Pay Salaries First");
         require(isWorker[_worker] == true,"Must be a worker");
+        require(address(this).balance == 0,"Contract must be empty");
         for(uint i = 0; i<= Workers.length;i++){
             if(_worker == Workers[i]){
                 Workers.pop();
