d8ce34cb2990e8597f12d072bb60edbdeced38f7
Akande Mayowa Oluwapelumi <mayoral13g@gmail.com>
2022-07-24 17:02:27 +0100

    Update TimedSalary.sol

--- contracts/TimedSalary.sol
+++ contracts/TimedSalary.sol
@@ -7,7 +7,6 @@ contract TimedSalary is Ownable{
     uint private timePaid;
     mapping(address => uint)balances;
     mapping(address => bool)isWorker;
-    mapping(address => bool)isPaid;
     mapping(address => uint)balanceRecieved;
     address[]Workers;
     bool Paid;
@@ -15,28 +14,31 @@ contract TimedSalary is Ownable{
 
     function RecieveETH()external payable returns(bool success){
      require(Workers.length != 0,"Add some workers first");
-     require(Paid == false,"Pay Salaries First");
-     for(uint i = 0; i<Workers.length;i++){
+     require(cooldown != 0,"Set a Cooldown First");
+     for(uint i = 0; i<=Workers.length;i++){
       balances[Workers[i]] = msg.value.div(Workers.length);
       balanceRecieved[Workers[i]] = balanceRecieved[Workers[i]].add(balances[Workers[i]]);
-     Paid = true;
+     PaySalary();
      }
      return true;
     }
     function SetCooldown(uint _value)external onlyOwner returns(bool success){
         require(_value != 0,"Cooldown cannot be 0");
-        _value = cooldown;
+        cooldown = _value;
+        return true;
     }
 
     function AddWorkers(address _worker)external onlyOwner returns(bool success){
+        require(Paid == false,"Pay Salaries First");
         require(isWorker[_worker] == false,"Already a worker");
         Workers.push(_worker);
         isWorker[_worker] = true;
         return true;
     }
     function RemoveWorker(address _worker)external onlyOwner returns(bool success){
+        require(Paid == false,"Pay Salaries First");
         require(isWorker[_worker] == true,"Must be a worker");
-        for(uint i = 0; i<Workers.length;i++){
+        for(uint i = 0; i<= Workers.length;i++){
             if(_worker == Workers[i]){
                 Workers.pop();
                 Workers.length-1;
@@ -52,23 +54,18 @@ contract TimedSalary is Ownable{
         return balances[_worker];
     }
 
-    function PaySalary()external onlyOwner returns(bool success){
-        require(Paid == true,"Deposit First");
+    function PaySalary()internal returns(bool success){
         require(cooldown != 0,"Set Cooldown First");
-        if(block.timestamp >= timePaid + cooldown){
-            for(uint i = 0; i<Workers.length;i++){
+        if(block.timestamp >= timePaid.add(cooldown)){
+            for(uint i = 0; i <= Workers.length;i++){
                 balanceRecieved[Workers[i]] = balanceRecieved[Workers[i]].add(balances[Workers[i]]);
                 balances[Workers[i]] = 0;
-                Paid = false;
+                Paid = true;
                 payable(Workers[i]).send(balances[Workers[i]]);
-                isPaid[Workers[i]] = true;
                 timePaid = block.timestamp;
         }
         return true;
     }
-    }
-    function isSalaryPaid()public view returns(bool){
-        return isPaid[msg.sender];
     }
     function ETHBalance()public view returns(uint){
         return msg.sender.balance;
